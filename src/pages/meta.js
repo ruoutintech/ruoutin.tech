f4dbdc70666cefca50a0548e70f22713cb6dff77
Hugo De Moraes <hdemoraes@squareup.com>
2021-01-04 15:04:15 -0500

    SEO tooling

--- src/pages/meta.js
+++ src/pages/meta.js
@@ -6,25 +6,11 @@ class MetaTags extends React.Component {
     return (
       <Helmet>
         <html lang="en" />
-        <meta charSet="utf-8" />
-        <meta
-          name="keywords"
-          content="Rutgers, Out In Tech, RU, OIT, LGBTQ+, Gay, Queer, Ally, Club"
-        />
-        <meta
-          name="description"
-          content="Website for Rutgers Out In Tech Club"
-        />
-        <meta name="author" content="Hugo De Moraes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="canonical" href="https://ruoutin.tech/" />
-
-        {/* <!-- Primary Meta Tags --> */}
         <title>Uniting LGBTQ+ in Tech @ Rutgers</title>
         <meta name="title" content="Uniting LGBTQ+ in Tech @ Rutgers" />
         <meta
           name="description"
-          content="Website for Rutgers Out In Tech Club"
+          content="Official site of the Rutgers Out In Tech Club"
         />
 
         {/* <!-- Open Graph / Facebook --> */}
@@ -33,9 +19,9 @@ class MetaTags extends React.Component {
         <meta property="og:title" content="Uniting LGBTQ+ in Tech @ Rutgers" />
         <meta
           property="og:description"
-          content="Website for Rutgers Out In Tech Club"
+          content="Official site of the Rutgers Out In Tech Club"
         />
-        <meta property="og:image" content="" />
+        <meta property="og:image" content="../../assets/img/website-screenshot.png" />
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
@@ -46,12 +32,12 @@ class MetaTags extends React.Component {
         />
         <meta
           property="twitter:description"
-          content="Website for Rutgers Out In Tech Club"
+          content="Official site of the Rutgers Out In Tech Club"
         />
-        <meta property="twitter:image" content="" />
+        <meta property="twitter:image" content="../../assets/img/website-screenshot.png" />
       </Helmet>
     )
   }
 }
 
-export default MetaTags;
+export default MetaTags
